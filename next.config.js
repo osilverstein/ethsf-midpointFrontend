f5c8cc12dc3a569cf60689734eba98c14ac76b28
osilverstein <silversteinoliver@gmail.com>
2022-11-06 05:13:12 -0800

    add files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+//add react.semantic-ui.com to the list of domains that can be loaded from for images
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ['react.semantic-ui.com', 'twitter.com', 'pbs.twimg.com'],
+  },
+  ...nextConfig,
+}
